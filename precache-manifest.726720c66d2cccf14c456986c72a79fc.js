7bc9213357bbd899732d699ddad8800f9e643188
Chase <ChaseEWhitney@gmail.com>
2020-03-13 12:59:34 -0500

    Updates

---
+++ precache-manifest.726720c66d2cccf14c456986c72a79fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14adc5fd6e897fa70398eccbb26faf97",
+    "url": "/portfolioPage/index.html"
+  },
+  {
+    "revision": "58d2cb05ef395805df02",
+    "url": "/portfolioPage/static/css/main.4666bc10.chunk.css"
+  },
+  {
+    "revision": "f359c1bd7a8055a53ab7",
+    "url": "/portfolioPage/static/js/2.8ae443d9.chunk.js"
+  },
+  {
+    "revision": "ddfd09a9397ea336a2550edb6da27f94",
+    "url": "/portfolioPage/static/js/2.8ae443d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d2cb05ef395805df02",
+    "url": "/portfolioPage/static/js/main.78246768.chunk.js"
+  },
+  {
+    "revision": "5c769a5dc3108b19bea0",
+    "url": "/portfolioPage/static/js/runtime-main.ea5496ea.js"
+  },
+  {
+    "revision": "3cfd7c52574df0ace1d3c20748b17614",
+    "url": "/portfolioPage/static/media/ctr-dashboard.3cfd7c52.png"
+  },
+  {
+    "revision": "1490df03740c9faa10da00a848aeeaf8",
+    "url": "/portfolioPage/static/media/ctr-dropdown.1490df03.png"
+  },
+  {
+    "revision": "5c9e0408d00d258c7c473b69c40176f8",
+    "url": "/portfolioPage/static/media/dndspells.5c9e0408.png"
+  },
+  {
+    "revision": "38a83c19a904e49968b87356a5a6303d",
+    "url": "/portfolioPage/static/media/riotapi.38a83c19.png"
+  }
+]);
